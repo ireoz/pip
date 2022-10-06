ef3c04aa0c5d8091b40a523b13322af4b44a3dac
ireoz <andrew.higgins2@hotmail.com>
2022-10-06 16:46:50 +0100

    create picture in picture functionality

---
+++ script.js
@@ -0,0 +1,37 @@
+const pipButton = document.getElementById('pip');
+const videoElement = document.getElementById('video');
+console.log(pipButton);
+
+
+// pipButton.addEventListener('click', ())
+
+
+async function callCaptureStream() {
+    try {
+        videoElement.srcObject = await navigator.mediaDevices.getDisplayMedia();
+
+        if ('pictureInPictureEnabled' in document) {
+            pipButton.classList.remove('hidden')
+            pipButton.disabled = false;
+          
+            pipButton.addEventListener('click', () => {
+              videoElement.requestPictureInPicture();
+            });
+          }
+        
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+pipButton.addEventListener('click', () => {
+    if (document.pictureInPictureElement) {
+      document
+        .exitPictureInPicture()
+        .catch(error => {
+        console.log(error);
+      })
+    }
+  });
+
+callCaptureStream();
