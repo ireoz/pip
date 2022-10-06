04cf340e9ca35a166d32bb05217a8bd153a4e9dd
ireoz <andrew.higgins2@hotmail.com>
2022-10-06 20:04:06 +0100

    followed tutorial and modified js script solution to be similar to tutorial

--- script.js
+++ script.js
@@ -1,36 +1,33 @@
-const pipButton = document.getElementById('pip');
+const pipButton = document.getElementById('pip'); // 
 const videoElement = document.getElementById('video');
 
 
-// pipButton.addEventListener('click', ())
 
 
-async function callCaptureStream() {
+// Prompt to select media stream, pass to video element then play on pip
+async function selectMediaStream() {
     try {
         videoElement.srcObject = await navigator.mediaDevices.getDisplayMedia();
-
-        if ('pictureInPictureEnabled' in document) {
-            pipButton.classList.remove('hidden')
-            pipButton.disabled = false;
-          
-            pipButton.addEventListener('click', () => {
-              videoElement.requestPictureInPicture();
-            });
-          }
+        videoElement.onloadedmetadata = () => {
+            videoElement.play();
+        }
         
     } catch (error) {
-        console.log(error);
+        console.log('whoops error here: ' + error);
     }
 }
 
-pipButton.addEventListener('click', () => {
-    if (document.pictureInPictureElement) {
-      document
-        .exitPictureInPicture()
-        .catch(error => {
-        console.log(error);
-      })
+pipButton.addEventListener('click', async () =>{
+    try {
+        // disable button
+        pipButton.disabled = true;
+        // start picture in picture
+        await videoElement.requestPictureInPicture();
+        // reset button
+        pipButton.disabled = false;
+      } catch (error) {
+        console.log('Oh no!, we got an error: ' + error);
     }
-  });
+})
 
-// callCaptureStream();
+selectMediaStream();
